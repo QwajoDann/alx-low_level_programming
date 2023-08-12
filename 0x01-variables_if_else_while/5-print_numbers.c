225f90b5e0ed358a8c238f906229614d54a9174c
qwajodann <louisefremm@gmail.com>
2023-08-11 21:50:33 -0700

    print

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'print bumbers of base 10'
+ * Return: Always 0
+ */
+int manin(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}
